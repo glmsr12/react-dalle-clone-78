4b48e7173dbfbc58023f8a5370f3a5e89a742c2b
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-20 05:40:17 -0700

    "search bar created"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const surpriseOptions = [
+    'A red fox crossing a road',
+    'A flock of crows flying over a trash can',
+    'A watermelon swimming in creek',
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <section className="search-section">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start with a detailed description{' '}
+          <span className="surprise">Surprise me</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section className="image-section"></section>
     </div>
   );
 }
